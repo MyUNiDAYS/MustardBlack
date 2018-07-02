6d93d9f33c7930909c5bdf89e9e44f1c5aa80a49
Andrew Bullock <trullock@gmail.com>
2018-07-02 09:08:30 +0100

    updated paket settings

--- src/MustardBlack.Hosting.AspNet/Properties/AssemblyInfo.cs
+++ src/MustardBlack.Hosting.AspNet/Properties/AssemblyInfo.cs
@@ -12,3 +12,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
+
--- src/MustardBlack.ViewEngines.Razor/Properties/AssemblyInfo.cs
+++ src/MustardBlack.ViewEngines.Razor/Properties/AssemblyInfo.cs
@@ -4,4 +4,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("MustardBlack.ViewEngines.Razor")]
 [assembly: CLSCompliant(true)]
+
 [assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
