40796cacc4f631e88daae1bb7c863dcc213a4369
Andrew Bullock <trullock@gmail.com>
2018-11-07 13:25:24 +0000

    saved proj

--- src/MustardBlack.Build.Views/RazorConfiguration.cs
+++ src/MustardBlack.Build.Views/RazorConfiguration.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Xml;
 using Microsoft.CodeDom.Providers.DotNetCompilerPlatform;
 using MustardBlack.ViewEngines.Razor;
