3b33b67cc451de6bf29de0ddab6ae1d5e3229e02
Andrew Bullock <trullock@gmail.com>
2019-03-11 15:48:34 +0000

    updated listbinder to be less mardy

--- src/MustardBlack/Handlers/Binding/Binders/ListBinder.cs
+++ src/MustardBlack/Handlers/Binding/Binders/ListBinder.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Text.RegularExpressions;
 using MustardBlack.Hosting;
 using MustardBlack.Routing;
 
@@ -13,7 +12,9 @@ namespace MustardBlack.Handlers.Binding.Binders
 	{
 		public bool CanBind(string name, Type type, IRequest request, RouteValues routeValues, object owner)
 		{
-			return type.IsOrDerivesFrom(typeof(IList<>)) && !type.IsArray;
+			return type.IsOrDerivesFrom(typeof(IList<>)) && !type.IsArray &&
+			       (request.HttpMethod == HttpMethod.Post || request.HttpMethod == HttpMethod.Put || request.HttpMethod == HttpMethod.Patch)
+			       && request.ContentType != null && (request.ContentType.MediaType == "multipart/form-data" || request.ContentType.MediaType == "application/x-www-form-urlencoded");
 		}
 
 		public BindingResult Bind(string name, Type type, IRequest request, RouteValues routeValues, bool isNestedCall, object owner)
@@ -21,39 +22,31 @@ namespace MustardBlack.Handlers.Binding.Binders
 			var prefix = name + "[";
 			var bindingErrors = new List<BindingError>();
 
-			if (request.HttpMethod == HttpMethod.Post || request.HttpMethod == HttpMethod.Put || request.HttpMethod == HttpMethod.Patch)
+			var genericArgument = type.GetGenericArguments()[0];
+
+			var list = Activator.CreateInstance(typeof(ExpandableList<>).MakeGenericType(genericArgument)) as IList;
+
+			foreach (var key in request.Form.AllKeys)
 			{
-				if (request.ContentType != null && (request.ContentType.MediaType == "multipart/form-data" || request.ContentType.MediaType == "application/x-www-form-urlencoded"))
+				if (key.StartsWith(prefix, true, CultureInfo.InvariantCulture))
 				{
-					var genericArgument = type.GetGenericArguments()[0];
+					var start = prefix.Length;
+					var end = key.IndexOf(']', start);
 
-					var list = Activator.CreateInstance(typeof(ExpandableList<>).MakeGenericType(genericArgument)) as IList;
+					var index = int.Parse(key.Substring(start, end - start));
 
-					foreach (var key in request.Form.AllKeys)
-					{
-						if (key.StartsWith(prefix, true, CultureInfo.InvariantCulture))
-						{
-							var start = prefix.Length;
-							var end = key.IndexOf(']', start);
+					var indexedName = name + "[" + index + "]";
+					var innerBinder = BinderCollection.FindBinderFor(indexedName, genericArgument, request, routeValues, owner);
+					var bind = innerBinder.Bind(indexedName, genericArgument, request, routeValues, true, owner);
 
-							var index = int.Parse(key.Substring(start, end - start));
+					list[index] = bind.Object;
 
-							var indexedName = name + "[" + index + "]";
-							var innerBinder = BinderCollection.FindBinderFor(indexedName, genericArgument, request, routeValues, owner);
-							var bind = innerBinder.Bind(indexedName, genericArgument, request, routeValues, true, owner);
-
-							list[index] = bind.Object;
-
-							if (bind.Result == BindingResult.ResultType.Failure)
-								bindingErrors.AddRange(bind.BindingErrors);
-						}
-					}
-
-					return new BindingResult(list, bindingErrors.ToArray(), bindingErrors.Any() ? BindingResult.ResultType.Failure : BindingResult.ResultType.Success);
+					if (bind.Result == BindingResult.ResultType.Failure)
+						bindingErrors.AddRange(bind.BindingErrors);
 				}
 			}
 
-			throw new NotImplementedException("Cannot bind a List from the query string");
+			return new BindingResult(list, bindingErrors.ToArray(), bindingErrors.Any() ? BindingResult.ResultType.Failure : BindingResult.ResultType.Success);
 		}
 
 		public BindingResult Bind(Type type, string name, string value, IRequest request, RouteValues routeValues, object owner)
