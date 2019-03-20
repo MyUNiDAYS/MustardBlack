b9828c52b19393700440ac81fb0b1698f34448ef
Sam Strong <sam.strong@myunidays.com>
2019-03-20 17:14:39 +0000

    Using Nuglify for minification after Babel (instead of YUI).

---
+++ src/MustardBlack.Assets.Nuglify/NuglifyJavascriptPreprocessor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MustardBlack.Assets.Javascript;
+using NUglify;
+
+namespace MustardBlack.Assets.Nuglify
+{
+    public class NuglifyJavascriptPreprocessor : IJavascriptPreprocessor
+    {
+	    public string Process(IEnumerable<AssetContent> assets)
+	    {
+		    // Check that the individual assets compress in on their own.
+		    foreach (var asset in assets)
+			    CompressJavascript(asset.Contents, asset.Path);
+		    
+		    var builder = new StringBuilder();
+		    foreach (var asset in assets)
+			    builder.AppendLine(asset.Contents);
+		    
+		    return CompressJavascript(builder.ToString(), null);
+	    }
+
+	    static string CompressJavascript(string javascriptString, string assetPath)
+	    {
+		    try
+		    {
+			    return Uglify.Js(javascriptString).Code;
+		    }
+		    catch (Exception e)
+		    {
+			    Console.WriteLine($"Nuglify Compression Error: {e.Message}: File: {assetPath}");
+			    throw;
+		    }
+	    }
+    }
+}
