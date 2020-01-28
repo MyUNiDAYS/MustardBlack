f37d54fc7830e56f3e3ef0260bcf8fc3bb710ca2
Oliver Dunthorne <oliver.dunthorne@myunidays.com>
2020-01-28 13:30:03 +0000

    outputs the particular files js error for YUI

--- src/MustardBlack.Assets.YuiCompressor/YuiJavascriptPreprocessor.cs
+++ src/MustardBlack.Assets.YuiCompressor/YuiJavascriptPreprocessor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Text;
 using MustardBlack.Assets.Javascript;
@@ -7,14 +8,38 @@ namespace MustardBlack.Assets.YuiCompressor
 {
 	public sealed class YuiJavascriptPreprocessor : IJavascriptPreprocessor
 	{
+		
 		public string Process(IEnumerable<AssetContent> assets)
 		{
+            var javaScriptCompressor = new JavaScriptCompressor{Encoding = Encoding.UTF8};
+
+            //check all assets
+            foreach (var asset in assets)
+                CompressJavascript(javaScriptCompressor, asset.Contents, asset.Path);
+
 			var builder = new StringBuilder();
 			foreach (var asset in assets)
 				builder.AppendLine(asset.Contents);
 
-			var javaScriptCompressor = new JavaScriptCompressor{Encoding = Encoding.UTF8};
-			return javaScriptCompressor.Compress(builder.ToString());
+            return CompressJavascript(javaScriptCompressor, builder.ToString(), null);
 		}
-	}
+
+        static string CompressJavascript(JavaScriptCompressor javaScriptCompressor, string javascriptString, string assetPath)
+        {
+            try
+            {
+                return javaScriptCompressor.Compress(javascriptString);
+            }
+            catch (EcmaScript.NET.EcmaScriptRuntimeException e)
+            {
+                Console.WriteLine($"YUI Compression Error: '{e.Message}'\nJavascript Syntax Error: '{e.LineSource}'\nLine {e.LineNumber}\nFile: {assetPath}");
+                throw;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"YUI Compression Error: {e.Message}: File: {assetPath}");
+                throw;
+            }
+        }
+    }
 }
