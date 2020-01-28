6fb087e812d2151b8c77065ff8ad2a0c1708329c
Oliver Dunthorne <oliver.dunthorne@myunidays.com>
2020-01-28 13:31:38 +0000

    tidyied up whitespace

--- src/MustardBlack.Assets.YuiCompressor/YuiJavascriptPreprocessor.cs
+++ src/MustardBlack.Assets.YuiCompressor/YuiJavascriptPreprocessor.cs
@@ -6,23 +6,22 @@ using Yahoo.Yui.Compressor;
 
 namespace MustardBlack.Assets.YuiCompressor
 {
-	public sealed class YuiJavascriptPreprocessor : IJavascriptPreprocessor
-	{
-		
-		public string Process(IEnumerable<AssetContent> assets)
-		{
-            var javaScriptCompressor = new JavaScriptCompressor{Encoding = Encoding.UTF8};
+    public sealed class YuiJavascriptPreprocessor : IJavascriptPreprocessor
+    {
+        public string Process(IEnumerable<AssetContent> assets)
+        {
+            var javaScriptCompressor = new JavaScriptCompressor {Encoding = Encoding.UTF8};
 
             //check all assets
             foreach (var asset in assets)
                 CompressJavascript(javaScriptCompressor, asset.Contents, asset.Path);
 
-			var builder = new StringBuilder();
-			foreach (var asset in assets)
-				builder.AppendLine(asset.Contents);
+            var builder = new StringBuilder();
+            foreach (var asset in assets)
+                builder.AppendLine(asset.Contents);
 
             return CompressJavascript(javaScriptCompressor, builder.ToString(), null);
-		}
+        }
 
         static string CompressJavascript(JavaScriptCompressor javaScriptCompressor, string javascriptString, string assetPath)
         {
